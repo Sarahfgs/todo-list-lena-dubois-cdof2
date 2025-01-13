294768c94eccb2a4d7b079cc74a03d3707e1324f
lenadub <lena_dubois@orange.fr>
2025-01-13 14:01:57 +0100

    Added the setup code to the file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='todo-list-lena-dubois-cdof2',
+ version='1.0',
+ author='lenadub',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
